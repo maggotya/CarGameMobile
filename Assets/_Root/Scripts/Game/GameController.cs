1c7312e7f4e2afd156007137cb50e64e82bb3564
Anton Kerp <anton_kerp@mail.ru>
2022-09-29 20:56:35 +0600

    Распределяем код GameController по методам

--- Assets/_Root/Scripts/Game/GameController.cs
+++ Assets/_Root/Scripts/Game/GameController.cs
@@ -10,22 +10,58 @@ namespace Game
 {
     internal class GameController : BaseController
     {
+        private readonly SubscriptionProperty<float> _leftMoveDiff;
+        private readonly SubscriptionProperty<float> _rightMoveDiff;
+
+        private readonly CarController _carController;
+        private readonly InputGameController _inputGameController;
+        private readonly AbilitiesController _abilitiesController;
+        private readonly TapeBackgroundController _tapeBackgroundController;
+
+
         public GameController(Transform placeForUi, ProfilePlayer profilePlayer)
         {
-            var leftMoveDiff = new SubscriptionProperty<float>();
-            var rightMoveDiff = new SubscriptionProperty<float>();
+            _leftMoveDiff = new SubscriptionProperty<float>();
+            _rightMoveDiff = new SubscriptionProperty<float>();
 
-            var tapeBackgroundController = new TapeBackgroundController(leftMoveDiff, rightMoveDiff);
+            _carController = CreateCarController();
+            _inputGameController = CreateInputGameController(profilePlayer, _leftMoveDiff, _rightMoveDiff);
+            _abilitiesController = CreateAbilitiesController(placeForUi, _carController);
+            _tapeBackgroundController = CreateTapeBackground(_leftMoveDiff, _rightMoveDiff);
+        }
+
+
+        private TapeBackgroundController CreateTapeBackground(SubscriptionProperty<float> leftMoveDiff, SubscriptionProperty<float> rightMoveDiff)
+        {
+            TapeBackgroundController tapeBackgroundController = new(leftMoveDiff, rightMoveDiff);
             AddController(tapeBackgroundController);
 
-            var inputGameController = new InputGameController(leftMoveDiff, rightMoveDiff, profilePlayer.CurrentCar);
+            return tapeBackgroundController;
+        }
+
+        private InputGameController CreateInputGameController(ProfilePlayer profilePlayer,
+            SubscriptionProperty<float> leftMoveDiff, SubscriptionProperty<float> rightMoveDiff)
+        {
+            InputGameController inputGameController = new(leftMoveDiff, rightMoveDiff, profilePlayer.CurrentCar);
             AddController(inputGameController);
 
-            var carController = new CarController();
+            return inputGameController;
+        }
+
+        private CarController CreateCarController()
+        {
+            CarController carController = new();
             AddController(carController);
 
-            var abilitiesController = new AbilitiesController(placeForUi, carController);
+            return carController;
+        }
+
+        private AbilitiesController CreateAbilitiesController(Transform placeForUi, IAbilityActivator abilityActivator)
+        {
+            AbilitiesController abilitiesController = new(placeForUi, abilityActivator);
             AddController(abilitiesController);
+
+            return abilitiesController;
         }
     }
 }
