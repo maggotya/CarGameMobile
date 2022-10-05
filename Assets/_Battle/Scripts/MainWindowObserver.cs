763f971fd181e9379d46378db5d00fe7d561bac0
Anton Kerp <anton_kerp@mail.ru>
2022-10-05 23:08:46 +0600

    Избавляемся от дублирования данных

--- Assets/_Battle/Scripts/MainWindowObserver.cs
+++ Assets/_Battle/Scripts/MainWindowObserver.cs
@@ -30,10 +30,6 @@ namespace BattleScripts
         [Header("Other Buttons")]
         [SerializeField] private Button _fightButton;
 
-        private int _allCountMoneyPlayer;
-        private int _allCountHealthPlayer;
-        private int _allCountPowerPlayer;
-
         private DataPlayer _money;
         private DataPlayer _heath;
         private DataPlayer _power;
@@ -106,33 +102,31 @@ namespace BattleScripts
         }
 
 
-        private void IncreaseMoney() => IncreaseValue(ref _allCountMoneyPlayer, DataType.Money);
-        private void DecreaseMoney() => DecreaseValue(ref _allCountMoneyPlayer, DataType.Money);
+        private void IncreaseMoney() => IncreaseValue(_money);
+        private void DecreaseMoney() => DecreaseValue(_money);
 
-        private void IncreaseHealth() => IncreaseValue(ref _allCountHealthPlayer, DataType.Health);
-        private void DecreaseHealth() => DecreaseValue(ref _allCountHealthPlayer, DataType.Health);
+        private void IncreaseHealth() => IncreaseValue(_heath);
+        private void DecreaseHealth() => DecreaseValue(_heath);
 
-        private void IncreasePower() => IncreaseValue(ref _allCountPowerPlayer, DataType.Power);
-        private void DecreasePower() => DecreaseValue(ref _allCountPowerPlayer, DataType.Power);
+        private void IncreasePower() => IncreaseValue(_power);
+        private void DecreasePower() => DecreaseValue(_power);
 
-        private void IncreaseValue(ref int value, DataType dataType) => AddToValue(ref value, 1, dataType);
-        private void DecreaseValue(ref int value, DataType dataType) => AddToValue(ref value, -1, dataType);
+        private void IncreaseValue(DataPlayer dataPlayer) => AddToValue(1, dataPlayer);
+        private void DecreaseValue(DataPlayer dataPlayer) => AddToValue(-1, dataPlayer);
 
-        private void AddToValue(ref int value, int addition, DataType dataType)
+        private void AddToValue(int addition, DataPlayer dataPlayer)
         {
-            value += addition;
-            ChangeDataWindow(value, dataType);
+            dataPlayer.Value += addition;
+            ChangeDataWindow(dataPlayer);
         }
 
 
-        private void ChangeDataWindow(int countChangeData, DataType dataType)
+        private void ChangeDataWindow(DataPlayer dataPlayer)
         {
-            DataPlayer dataPlayer = GetDataPlayer(dataType);
+            int value = dataPlayer.Value;
+            DataType dataType = dataPlayer.DataType;
             TMP_Text textComponent = GetTextComponent(dataType);
-            string text = $"Player {dataType:F} {countChangeData}";
-
-            dataPlayer.Value = countChangeData;
-            textComponent.text = text;
+            textComponent.text = $"Player {dataType:F} {value}";
 
             int enemyPower = _enemy.CalcPower();
             _countPowerEnemyText.text = $"Enemy Power {enemyPower}";
@@ -147,20 +141,11 @@ namespace BattleScripts
                 _ => throw new ArgumentException($"Wrong {nameof(DataType)}")
             };
 
-        private DataPlayer GetDataPlayer(DataType dataType) =>
-            dataType switch
-            {
-                DataType.Money => _money,
-                DataType.Health => _heath,
-                DataType.Power => _power,
-                _ => throw new ArgumentException($"Wrong {nameof(DataType)}")
-            };
-
 
         private void Fight()
         {
             int enemyPower = _enemy.CalcPower();
-            bool isVictory = _allCountPowerPlayer >= enemyPower;
+            bool isVictory = _power.Value >= enemyPower;
 
             string color = isVictory ? "#07FF00" : "#FF0000";
             string message = isVictory ? "Win" : "Lose";
