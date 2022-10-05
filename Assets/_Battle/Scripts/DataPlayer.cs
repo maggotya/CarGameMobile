8c33c355c6b1b2ef94e77eb21bd665bf7d60e8d5
Anton Kerp <anton_kerp@mail.ru>
2022-10-05 23:08:26 +0600

    Устраним дублирование в DataPlayer

--- Assets/_Battle/Scripts/DataPlayer.cs
+++ Assets/_Battle/Scripts/DataPlayer.cs
@@ -4,74 +4,79 @@ namespace BattleScripts
 {
     internal abstract class DataPlayer
     {
-        private string _titleData;
+        private readonly List<IEnemy> _enemies;
+
         private int _countMoney;
         private int _countHealth;
         private int _countPower;
 
-        private List<IEnemy> _enemies = new List<IEnemy>();
-
-        public string TitleData => _titleData;
+        public string TitleData { get; }
 
         public int Money
         {
             get => _countMoney;
-            set
-            {
-                if (_countMoney != value)
-                {
-                    _countMoney = value;
-                    Notify(DataType.Money);
-                }
-            }
+            set => SetValue(DataType.Money, value);
         }
 
         public int Health
         {
             get => _countHealth;
-            set
-            {
-                if (_countHealth != value)
-                {
-                    _countHealth = value;
-                    Notify(DataType.Health);
-                }
-            }
+            set => SetValue(DataType.Health, value);
         }
 
         public int Power
         {
             get => _countPower;
-            set
-            {
-                if (_countPower != value)
-                {
-                    _countPower = value;
-                    Notify(DataType.Power);
-                }
-            }
+            set => SetValue(DataType.Power, value);
         }
 
 
         protected DataPlayer(string titleData)
         {
-            _titleData = titleData;
+            TitleData = titleData;
+            _enemies = new List<IEnemy>();
         }
 
-        public void Attach(IEnemy enemy)
-        {
-            _enemies.Add(enemy);
-        }
 
-        public void Detach(IEnemy enemy)
-        {
-            _enemies.Remove(enemy);
-        }
+        public void Attach(IEnemy enemy) => _enemies.Add(enemy);
+        public void Detach(IEnemy enemy) => _enemies.Remove(enemy);
 
         protected void Notify(DataType dataType)
         {
             foreach (var investor in _enemies)
                 investor.Update(this, dataType);
         }
+
+
+        private void SetValue(DataType dataType, int value)
+        {
+            int currentValue = GetValue(dataType);
+            if (currentValue == value)
+                return;
+
+            switch (dataType)
+            {
+                case DataType.Money:
+                    _countMoney = value;
+                    break;
+                case DataType.Health:
+                    _countHealth = value;
+                    break;
+                case DataType.Power:
+                    _countPower = value;
+                    break;
+            }
+
+            Notify(dataType);
+        }
+
+        private int GetValue(DataType dataType) =>
+            dataType switch
+            {
+                DataType.Money => _countMoney,
+                DataType.Health => _countHealth,
+                DataType.Power => _countPower,
+                _ => default
+            };
     }
 }
