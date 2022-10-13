d3c7edae9c0a3516d2e8df3d70905b640bf2b552
Anton Kerp <anton_kerp@mail.ru>
2022-10-13 19:56:26 +0600

    Разбираем пример с методами расширения

---
+++ Assets/_Tween/Scripts/Examples/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Tween.Examples
+{
+    internal static class GameObjectExtensions
+    {
+        public static void Move(this GameObject go, Vector3 startPosition, Vector3 endPosition, float duration)
+        {
+            if (!go.TryGetComponent(out VectorLerpComponent component))
+                component = go.AddComponent<VectorLerpComponent>();
+
+            component.Play(startPosition, endPosition, duration);
+        }
+    }
+}
